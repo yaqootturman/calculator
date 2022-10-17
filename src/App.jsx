cfe2cbe29f0634a0afd5832604d3c66d92c7b7e0
yaqootturman <yaqoot.turman@gmail.com>
2022-10-17 21:10:07 +0300

    add some style

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import react, {useState} from 'react'
-import Result from './Components/Result';
+import react, { useState } from "react";
+import Result from "./Components/Result";
+import './Components/style.css'
 
-const App = ()=> {
-  const [statement, setStatement]=  useState([])
-  const [clicked, setClicked] = useState(false)
-  const [result, setResult] =useState(0)
+const App = () => {
+  const [statement, setStatement] = useState([]);
+  const [clicked, setClicked] = useState(false);
+  const [result, setResult] = useState(0);
 
   return (
-    <div >
-      <input value={result} onChange={()=>{}}></input>
-    { 
-     [1,2,3,4,5,6,7,8,9,0,'+', '-'].map((ele,index)=>{
-        return <button key ={index} onClick={(e)=>{
-          setStatement([...statement, e.target.innerText])         
-        }}>{ele}</button>
-      })
-    }
-    <button onClick={()=>{
-      setClicked(!clicked)
-    }}>
-      =
-    </button>
-    <button onClick={(e)=>{
-      setResult(0)
-      setStatement([])
-    }}>
-      C
-    </button>
-    {clicked ? <Result statement={statement} setResult={setResult} setStatement={setStatement}/> :null}
+    <div class="cal-body">
+      <input class="result-area" disabled value={result} onChange={() => {}}></input>
+      <div class="numbers-area">
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "+", "-"].map((ele, index) => {
+        return (
+          <button
+            class="number"
+            key={index}
+            onClick={(e) => {
+              setStatement([...statement, e.target.innerText]);
+            }}
+          >
+            {ele}
+          </button>
+        );
+      })}
+      </div>
+      <button
+        class="equal-sign"
+        onClick={() => {
+          setClicked(!clicked);
+        }}
+      >
+        =
+      </button>
+      <button
+        class="reset"
+        onClick={(e) => {
+          setResult(0);
+          setStatement([]);
+        }}
+      >
+        C
+      </button>
+      {clicked ? (
+        <Result
+          statement={statement}
+          setResult={setResult}
+          setStatement={setStatement}
+        />
+      ) : null}
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,27 +1,35 @@
-import react, {useState} from 'react'
+import react, { useState } from "react";
+import './style.css'
 
-const summation =(statement)=>{
-    let firstOperand =Number(statement.substr(0, statement.indexOf('+')))
-    let secondOperand =Number(statement.substr(statement.indexOf('+')+1, statement.length))
-    return firstOperand+secondOperand
-}
+const summation = (statement) => {
+  let firstOperand = Number(statement.substr(0, statement.indexOf("+")));
+  let secondOperand = Number(
+    statement.substr(statement.indexOf("+") + 1, statement.length)
+  );
+  return firstOperand + secondOperand;
+};
 
-const subtraction =(statement)=>{
-    let firstOperand =Number(statement.substr(0, statement.indexOf('-')))
-    let secondOperand =Number(statement.substr(statement.indexOf('-')+1, statement.length))
-    return firstOperand-secondOperand
-}
+const subtraction = (statement) => {
+  let firstOperand = Number(statement.substr(0, statement.indexOf("-")));
+  let secondOperand = Number(
+    statement.substr(statement.indexOf("-") + 1, statement.length)
+  );
+  return firstOperand - secondOperand;
+};
 
-const Result = (props)=>{
-
-    let {statement,setResult} =props
-    statement =statement.join('')
-    return (
-        <>
-       {
-        setResult(statement.includes('+')? summation(statement) :statement.includes('-') ? subtraction(statement): null)
-       }
-        </>
-    )
-}
-export default Result
+const Result = (props) => {
+  let { statement, setResult } = props;
+  statement = statement.join("");
+  return (
+    <>
+      {setResult(
+        statement.includes("+")
+          ? summation(statement)
+          : statement.includes("-")
+          ? subtraction(statement)
+          : null
+      )}
+    </>
+  );
+};
+export default Result;
